1a4c25d1fb501bc7597bede031e159dad9616915
unknown <User@SHIK029.Shikma.local>
2019-11-20 14:47:28 +0200

    version 2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Classroom");
         }
     }
 }
